3c939df0f2a31eb2a6dcf865095094ea8211cb1e
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2018-04-14 20:15:39 +0200

    chore: bump all the dependencies to their latest versions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject github-repo-widget "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.908"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.238"]
                  [reagent "0.7.0"]
                  [re-frame "0.10.5"]]
 
@@ -18,14 +18,14 @@
 
   :profiles
   {:dev
-   {:dependencies [[binaryage/devtools "0.9.4"]
-                   [day8.re-frame/re-frame-10x "0.3.0"]
-                   [day8.re-frame/tracing "0.5.0"]
-                   [figwheel-sidecar "0.5.13"]
+   {:dependencies [[binaryage/devtools "0.9.10"]
+                   [day8.re-frame/re-frame-10x "0.3.2"]
+                   [day8.re-frame/tracing "0.5.1"]
+                   [figwheel-sidecar "0.5.15"]
                    [com.cemerick/piggieback "0.2.2"]]
 
-    :plugins      [[lein-figwheel "0.5.13"]]}
-   :prod { :dependencies [[day8.re-frame/tracing-stubs "0.5.0"]]}}
+    :plugins      [[lein-figwheel "0.5.15"]]}
+   :prod { :dependencies [[day8.re-frame/tracing-stubs "0.5.1"]]}}
 
   :cljsbuild
   {:builds

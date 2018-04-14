8609368ab6ff2eae5ec043032dba283723ed96b9
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2018-04-14 20:18:17 +0200

    feat: add semantic-ui-react

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [reagent "0.7.0"]
-                 [re-frame "0.10.5"]]
+                 [re-frame "0.10.5"]
+                 [cljsjs/semantic-ui-react "0.78.2-0"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]]
 
